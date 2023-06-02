34c271e502f401d8100ba9a934b536c1543e7aa5
Ansari Umer <umer37454@gmail.com>
2023-06-02 19:15:41 +0530

    Array Operations

--- Arrays_01/deletion.java
+++ Arrays_01/deletion.java
@@ -3,29 +3,76 @@ package Arrays_01;
 import java.util.Arrays;
 
 public class deletion {
-    public static void main(String[] args){
-        int[] arr = {54,25,63,87,56,14};
+    public static void main(String[] args) {
+        int[] arr = {54, 25, 63, 87, 56, 14, 47, 36};
 
-        int element = 25;
+        int element = 36;
 
-        int[] result = delete(arr,element);
+        int[] result = delete(arr, element);
         System.out.println(Arrays.toString(result));
+
+        delete2(arr, element);
+        delete3(arr, element);
     }
 
-    static int[] delete(int[] arr, int element){
+    //by copying in another array
+    static int[] delete(int[] arr, int element) {
         int[] result = new int[arr.length - 1];
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == element){
-                for(int j = i; j < arr.length - 1; j++){
-                    arr[j] = arr[j+1];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                for (int j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
                 }
             }
         }
 
-        for(int k = 0; k < result.length; k++){
+        for (int k = 0; k < result.length; k++) {
             result[k] = arr[k];
         }
         return result;
+    }
+
+    //by swapping in same array without printing last element
+    static void delete2(int[] arr, int element) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == element) {
+                for (int j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+            }
+        }
+
+        System.out.print("[");
+        for(int k = 0; k < arr.length - 1; k++){
+            System.out.print(" " + arr[k] + " ");
+        }
+        System.out.println("]");
+    }
+
+    //by copying in another array - 2nd method
+    static void delete3(int[] arr, int element) {
+        int[] result = new int[arr.length - 1];
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                index = i;
+            }
+        }
+
+        for (int j = 0; j < result.length; j++) {
+            if (j < index) {
+                result[j] = arr[j];
+            } else if (j == index) {
+                result[j] = arr[j + 1];
+            } else {
+                result[j] = arr[j];
+            }
+        }
+
+        for (int k : result) {
+            System.out.print(k + " ");
+        }
     }
 }
--- Arrays_01/reverseArray.java
+++ Arrays_01/reverseArray.java
@@ -7,8 +7,9 @@ import java.util.Arrays;
 public class reverseArray {
     public static void main(String[] args) {
         int[] arr = {4, 8, 5, 6, 2, 7, 9, 6, 2, 15, 96, 25};
+        int[] arr2 = {4, 8, 5, 6, 2, 7, 9, 6, 2, 15, 96, 25};
         reverse(arr); // passing by reference
-        System.out.println(Arrays.toString(arr));
+        reverse2(arr2); // passing by reference
     }
 
     public static void reverse(int[] arr) {
@@ -24,5 +25,23 @@ public class reverseArray {
             start++;
             end--;
         }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse2(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
